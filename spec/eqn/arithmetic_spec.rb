d573c118d57255a7a494a8441d96b3e9eac0f410
Zach Schneider <zach@schneid.io>
2017-05-03 23:23:49 -0400

    Add newlines for test readability.

--- spec/eqn/arithmetic_spec.rb
+++ spec/eqn/arithmetic_spec.rb
@@ -6,12 +6,14 @@ describe Eqn do
       expect(Eqn::Calculator.calc('-3+1')).to eq(-2)
       expect(Eqn::Calculator.calc('-3+-1')).to eq(-4)
     end
+
     it 'performs basic subtraction' do
       expect(Eqn::Calculator.calc('5-3')).to eq(2)
       expect(Eqn::Calculator.calc('5--3')).to eq(8)
       expect(Eqn::Calculator.calc('-5-3')).to eq(-8)
       expect(Eqn::Calculator.calc('-5--3')).to eq(-2)
     end
+
     it 'performs basic multiplication' do
       expect(Eqn::Calculator.calc('2*4')).to eq(8)
       expect(Eqn::Calculator.calc('2*.4')).to eq(0.8)
@@ -19,12 +21,14 @@ describe Eqn do
       expect(Eqn::Calculator.calc('-2*4')).to eq(-8)
       expect(Eqn::Calculator.calc('-2*-4')).to eq(8)
     end
+
     it 'performs basic division' do
       expect(Eqn::Calculator.calc('10/5')).to eq(2)
       expect(Eqn::Calculator.calc('10/-5')).to eq(-2)
       expect(Eqn::Calculator.calc('-10/5')).to eq(-2)
       expect(Eqn::Calculator.calc('-10/-5')).to eq(2)
     end
+
     it 'performs basic exponentiation' do
       expect(Eqn::Calculator.calc('2^3')).to eq(8)
       expect(Eqn::Calculator.calc('2^-3')).to eq(0.125)
@@ -38,54 +42,67 @@ describe Eqn do
       expect(Eqn::Calculator.calc('1 + 2 * 3')).to eq(7)
       expect(Eqn::Calculator.calc('2 * 2 + 3')).to eq(7)
     end
+
     it 'performs multiplication before subtraction' do
       expect(Eqn::Calculator.calc('10 - 2 * 3')).to eq(4)
       expect(Eqn::Calculator.calc('10 * 2 - 3')).to eq(17)
     end
+
     it 'performs division before addition' do
       expect(Eqn::Calculator.calc('1 + 6 / 3')).to eq(3)
       expect(Eqn::Calculator.calc('6 / 3 + 1')).to eq(3)
     end
+
     it 'performs division before subtraction' do
       expect(Eqn::Calculator.calc('4 - 6 / 3')).to eq(2)
       expect(Eqn::Calculator.calc('6 / 3 - 1')).to eq(1)
     end
+
     it 'performs exponentiation before multiplication' do
       expect(Eqn::Calculator.calc('2 ^ 2 * 3')).to eq(12)
       expect(Eqn::Calculator.calc('3 * 2 ^ 2')).to eq(12)
     end
+
     it 'performs exponentiation before division' do
       expect(Eqn::Calculator.calc('2 ^ 3 / 2')).to eq(4)
       expect(Eqn::Calculator.calc('2 / 2 ^ 3')).to eq(0.25)
     end
+
     it 'performs exponentiation before addition' do
       expect(Eqn::Calculator.calc('2 ^ 2 + 3')).to eq(7)
       expect(Eqn::Calculator.calc('3 + 2 ^ 2')).to eq(7)
     end
+
     it 'performs exponentiation before subtraction' do
       expect(Eqn::Calculator.calc('2 ^ 2 - 3')).to eq(1)
       expect(Eqn::Calculator.calc('5 - 2 ^ 2')).to eq(1)
     end
+
     it 'performs parenthesized addition before multiplication' do
       expect(Eqn::Calculator.calc('(2 + 2) * 3')).to eq(12)
       expect(Eqn::Calculator.calc('3 * (2 + 2)')).to eq(12)
     end
+
     it 'performs parenthesized subtraction before multiplication' do
       expect(Eqn::Calculator.calc('(5 - 2) * 3')).to eq(9)
       expect(Eqn::Calculator.calc('3 * (5 - 2)')).to eq(9)
     end
+
     it 'performs parenthesized addition before division' do
       expect(Eqn::Calculator.calc('(2 + 4) / 3')).to eq(2)
       expect(Eqn::Calculator.calc('12 / (2 + 4)')).to eq(2)
     end
+
     it 'performs parenthesized subtraction before division' do
       expect(Eqn::Calculator.calc('(8 - 2) / 3')).to eq(2)
       expect(Eqn::Calculator.calc('12 / (8 - 2)')).to eq(2)
     end
+
     it 'performs parenthesized addition before exponentiation' do
       expect(Eqn::Calculator.calc('(2 + 2) ^ 3')).to eq(64)
       expect(Eqn::Calculator.calc('3 ^ (2 + 1)')).to eq(27)
     end
+
     it 'performs parenthesized subtraction before exponentiation' do
       expect(Eqn::Calculator.calc('(5 - 2) ^ 3')).to eq(27)
       expect(Eqn::Calculator.calc('3 ^ (5 - 2)')).to eq(27)
--- spec/eqn/parse_spec.rb
+++ spec/eqn/parse_spec.rb
@@ -3,72 +3,95 @@ describe Eqn do
     it 'parses a positive integer' do
       expect(Eqn::Calculator.calc('1')).to eq(1)
     end
+
     it 'parses a negative integer' do
       expect(Eqn::Calculator.calc('-1')).to eq(-1)
     end
+
     it 'parses a positive float' do
       expect(Eqn::Calculator.calc('1.5')).to eq(1.5)
     end
+
     it 'parses a negative float' do
       expect(Eqn::Calculator.calc('-1.5')).to eq(-1.5)
     end
+
     it 'parses a positive integer positive integer exponent' do
       expect(Eqn::Calculator.calc('2e5')).to eq(200_000)
     end
+
     it 'parses a positive integer negative integer exponent' do
       expect(Eqn::Calculator.calc('2e-5')).to eq(0.00002)
     end
+
     it 'parses a positive float positive integer exponent' do
       expect(Eqn::Calculator.calc('2.5e5')).to eq(250_000)
     end
+
     it 'parses a positive float negative integer exponent' do
       expect(Eqn::Calculator.calc('2.5e-5')).to eq(0.000025)
     end
+
     it 'parses a negative integer positive integer exponent' do
       expect(Eqn::Calculator.calc('-2e5')).to eq(-200_000)
     end
+
     it 'parses a negative integer negative integer exponent' do
       expect(Eqn::Calculator.calc('-2e-5')).to eq(-0.00002)
     end
+
     it 'parses a negative float positive integer exponent' do
       expect(Eqn::Calculator.calc('-2.5e5')).to eq(-250_000)
     end
+
     it 'parses a negative float negative integer exponent' do
       expect(Eqn::Calculator.calc('-2.5e-5')).to eq(-0.000025)
     end
+
     it 'parses a positive integer positive float exponent' do
       expect(Eqn::Calculator.calc('2e1.5')).to eq(63.245553203367585)
     end
+
     it 'parses a positive integer negative float exponent' do
       expect(Eqn::Calculator.calc('2e-1.5')).to eq(0.06324555320336758)
     end
+
     it 'parses a positive float positive float exponent' do
       expect(Eqn::Calculator.calc('2.5e1.5')).to eq(79.05694150420948)
     end
+
     it 'parses a positive float negative float exponent' do
       expect(Eqn::Calculator.calc('2.5e-1.5')).to eq(0.07905694150420947)
     end
+
     it 'parses a negative integer positive float exponent' do
       expect(Eqn::Calculator.calc('-2e1.5')).to eq(-63.245553203367585)
     end
+
     it 'parses a negative integer negative float exponent' do
       expect(Eqn::Calculator.calc('-2e-1.5')).to eq(-0.06324555320336758)
     end
+
     it 'parses a negative float positive float exponent' do
       expect(Eqn::Calculator.calc('-2.5e1.5')).to eq(-79.05694150420948)
     end
+
     it 'parses a negative float negative float exponent' do
       expect(Eqn::Calculator.calc('-2.5e-1.5')).to eq(-0.07905694150420947)
     end
+
     it 'parses a positive float with no leading number' do
       expect(Eqn::Calculator.calc('.1')).to eq(0.1)
     end
+
     it 'parses a negative float with no leading number' do
       expect(Eqn::Calculator.calc('-.1')).to eq(-0.1)
     end
+
     it 'parses a positive exponent with no leading number' do
       expect(Eqn::Calculator.calc('.1e.1')).to eq(0.12589254117941673)
     end
+
     it 'parses a negative exponent with no leading number' do
       expect(Eqn::Calculator.calc('.1e-.1')).to eq(0.07943282347242815)
     end
