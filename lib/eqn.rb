860d51b88de04383732b75786e3b5cc25e076337
Zach Schneider <zach@schneid.io>
2017-05-03 22:52:04 -0400

    Clean up parser, remove class var to ensure thread safety.

--- lib/eqn.rb
+++ lib/eqn.rb
@@ -1,2 +1,7 @@
 require 'treetop'
+
+# Load the parser from the Treetop grammar.
+Treetop.load(File.join(__dir__, 'eqn.treetop'))
+
+# Load eqn classes.
 Dir.glob File.join(__dir__, 'eqn', '**', '*.rb'), &method(:require)
--- lib/eqn/parser.rb
+++ lib/eqn/parser.rb
@@ -1,16 +1,14 @@
 module Eqn
   class Parser
-    # Load the Treetop grammar from the grammar.
-    Treetop.load(File.join(File.dirname(__dir__), 'eqn.treetop'))
-    @@parser = EqnParser.new
-
     class << self
       def parse(data)
+        parser = EqnParser.new
+
         # Pass the data over to the parser instance.
-        tree = @@parser.parse(data.downcase)
+        tree = parser.parse(data.downcase)
 
         # Raise any errors.
-        raise ParseError, "Parse error at offset: #{@@parser.index}" + @@parser.failure_reason if tree.nil?
+        raise ParseError, "Parse error at offset: #{parser.index} -- #{parser.failure_reason}" if tree.nil?
 
         # Remove extraneous nodes and return tree.
         clean_tree(tree)
@@ -21,7 +19,7 @@ module Eqn
       def clean_tree(root_node)
         return if root_node.elements.nil?
         root_node.elements.delete_if { |node| node.class == Treetop::Runtime::SyntaxNode }
-        root_node.elements.each { |node| clean_tree(node) }
+        root_node.elements.each(&method(:clean_tree))
         root_node
       end
     end
