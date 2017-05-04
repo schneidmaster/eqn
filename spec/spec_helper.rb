51457f963c18412cd71355ff66b0d4ad906f9425
Zach Schneider <zach@schneid.io>
2017-05-03 22:38:16 -0400

    Exclude specs from coverage report.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start do
-  add_filter 'lib/eqn/engine.rb'
+  add_filter '/lib/eqn/engine.rb'
+  add_filter '/spec'
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
