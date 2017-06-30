0b95e8243b50e4b6f26a50acc8c7a837c2e12dcf
Zach Schneider <zach@schneid.io>
2017-06-30 13:35:22 -0400

    Improve readability of methods in Eqn::Calculator.

--- lib/eqn/calculator.rb
+++ lib/eqn/calculator.rb
@@ -14,34 +14,60 @@ module Eqn
       end
     end
 
-    def method_missing(method, *args) # rubocop:disable AbcSize
-      if %i(calculate calc valid?).include?(method)
+    def method_missing(method, *args)
+      if delegated_method?(method)
         self.class.send(method, @eqn, @vars)
-      elsif (match = method.to_s.match(/^[A-Za-z]+=$/))
-        @vars[match.to_s.delete('=').to_sym] = args.first
-      elsif (var = method.to_s.match(/^[A-Za-z]+$/).to_s.to_sym) && @vars.key?(var)
-        @vars[var]
+      elsif match_setter?(method)
+        @vars[setter_key(method)] = args.first
+      elsif match_getter?(method)
+        @vars[getter_key(method)]
       else
         super
       end
     end
 
-    def respond_to_missing?(method, _p2 = false)
-      %i(calculate calc valid?).include?(method) ||
-        method.to_s.match(/^[A-Za-z]+=$/) ||
-        (var = method.to_s.match(/^[A-Za-z]+$/).to_s.to_sym) && @vars.key?(var)
+    def respond_to_missing?(method, _include_private = false)
+      delegated_method?(method) || match_setter?(method) || match_getter?(method)
+    end
+
+    private
+
+    def delegated_method?(method)
+      %i(calculate calc valid?).include?(method.to_sym)
+    end
+
+    def setter_key(method)
+      match = method.to_s.match(/^[A-Za-z]+=$/)
+      return unless match
+      match.to_s.delete('=').to_sym
+    end
+
+    def match_setter?(method)
+      !setter_key(method).nil?
+    end
+
+    def getter_key(method)
+      match = method.to_s.match(/^[A-Za-z]+$/)
+      return if match.nil?
+      key = match.to_s.to_sym
+      return unless @vars.key?(key)
+      key
+    end
+
+    def match_getter?(method)
+      !getter_key(method).nil?
     end
 
     class << self
-      def calculate(data, vars = {})
-        result = Parser.parse(data).value(vars)
+      def calculate(equation, vars = {})
+        result = Parser.parse(equation).value(vars)
         raise ZeroDivisionError if result.is_a?(Float) && (result.abs == Float::INFINITY || result.nan?)
         result
       end
       alias calc calculate
 
-      def valid?(data, vars = {})
-        calc(data, vars)
+      def valid?(equation, vars = {})
+        calculate(equation, vars)
         true
       rescue EqnError
         false
